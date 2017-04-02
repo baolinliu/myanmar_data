84729beff052d74e5e2c194d07f64668e33eee0f
Jason Chiang <jachian@Jasons-MacBook.local>
2017-04-01 17:54:41 -0700

    Data source aggregation is working

--- data_by_township.py
+++ data_by_township.py
@@ -17,10 +17,10 @@ def get_data():
 	# Township population data
 	# Sourced via: http://themimu.info/doc-type/census-baseline-data
 	township_full = pd.read_excel('BaselineData_Census_Dataset_Township_MIMU_16Jun2016_ENG.xlsx')
-	township_full.columns = ['t_' + str(i) for i in xrange(township.shape[1])]
+	township_full.columns = ['t_' + str(i) for i in xrange(township_full.shape[1])]
 
 	cols = ['t_4', 't_5', 't_6', 't_24', 't_54', 't_66', 't_78']
-	town = pd.DataFrame(township[cols][2:].values)
+	town = pd.DataFrame(township_full[cols][2:].values)
 	town.columns = ['pcode_ts', 'township_name', 'pop_total', 'urban_perc',\
 	'literacy_perc_total', 'literacy_perc_urban', 'literacy_perc_rural']
 
@@ -31,11 +31,42 @@ def get_data():
 	mean_hh = pd.DataFrame(mean_hh_full[cols].values)
 	mean_hh.columns = cols
 
+	# Census data for light sources
+	# Sourced via: https://data.opendevelopmentmekong.net/dataset/2014-myanmar-census
+	cols = ['pcode_ts', 'light_total', 'electricity', 'kerosene', 'candle', 'lbattery', 'generator', 'water', 'solar', 'other']
+	light_source_full = pd.read_csv('Censussourceoflighttsp.csv')
+	light_source = pd.DataFrame(light_source_full[cols].values)
+	light_source.columns = cols
+
+	# Census transportation data
+	# Sourced via: https://data.opendevelopmentmekong.net/dataset/2014-myanmar-census
+	cols = ['pcode_ts', 'trans_t', 'trans_car', 'trans_mcyc', 'trans_bicyc', 'trans_4wheel', 'trans_canoe', 'trans_mboat', 'trans_cart']
+	transportation_full = pd.read_csv('Censustransportationtsp.csv')
+	transportation = pd.DataFrame(transportation_full[cols].values)
+	transportation.columns = cols
+
+	# Census home ownership data
+	# Sourced via: https://data.opendevelopmentmekong.net/dataset/2014-myanmar-census
+	cols = ['pcode_ts', 'ownshp_t', 'ownshp_own', 'ownshp_rent', 'ownshp_free', 'ownshp_gov', 'ownshp_com', 'ownshp_oth']
+	home_ownership_full = pd.read_csv('Censusownershipofhousingtsp.csv')
+	home_ownership = pd.DataFrame(home_ownership_full[cols].values)
+	home_ownership.columns = cols
+
+	# Census communication data
+	# Sourced via: 
+	cols = ['pcode_ts', 'com_t', 'com_radio', 'com_tv', 'com_lline', 'com_mob', 'com_comp', 'com_int']
+	communication_full = pd.read_csv('Censuscommuniationtsp.csv')
+	communication_full[cols].head()
+	communication = pd.DataFrame(communication_full[cols].values)
+	communication.columns = cols
+
 	# Merging
-	tables = [town, mean_hh]
-	df_all = pcodes.copy()
+	tables = [mean_hh, light_source, transportation, home_ownership, communication]
+	df_all = town.copy()
 	for df in tables:
-		df_all = pd.merge(df_all, df, on='pcode_ts')
+		df_all = pd.merge(df_all, df, how='left', on='pcode_ts')
+
+	return df_all
 
 def main():
 	pass
